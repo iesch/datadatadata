169e827649648fa1065dee4f0b1829ff40c6f3b2
Igone Schotman <i.e.schotman@students.uu.nl>
2026-01-21 11:39:35 +0100

    idk

--- Mapping/ggplot_mapping.R
+++ Mapping/ggplot_mapping.R
@@ -12,4 +12,12 @@ ggplot() +
     color = "black", fill = "lightgreen", linewidth = 0.2 )+
   theme(panel.background = element_rect(fill = "lightblue"),
       panel.grid.minor = element_line(color="lightblue"),
-      panel.grid.major= element_line(color="lightblue"))
+      panel.grid.major= element_line(color="lightblue"),
+      axis.ticks.y = element_blank(),
+      axis.ticks.x = element_blank()) +
+  scale_x_continuous(labels = NULL) +
+  scale_y_continuous(labels = NULL) +
+  labs(x = NULL, y = NULL)
+ 
+#data_19 <- read_csv
+#data_20 <- read_csv
